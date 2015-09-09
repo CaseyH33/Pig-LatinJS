d2a2f818ff07762ba3af3ed58084c6ec4943da76
Jordan Johansen & Casey Heitz <student@epicodus.com>
2015-09-09 11:21:26 -0700

    Add passing tests for words with y

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,40 @@
 var pigLatin = function(string) {
-  for(var i=0; i<string.length; i++) {
-    var currentLetter = string.charAt(0);
-    if (vowelCheck(currentLetter)) {
-      break;
-    } else if (currentLetter.toLowerCase() === "q" && string.charAt(1).toLowerCase() === "u") {
-      string = string.slice(2, string.length);
-      string += "qu";
-    } else {
-      string = string.slice(1, string.length);
-      string += currentLetter;
-    }
-  }
-  return string += "ay";
+  var word_array = wordSeparator(string);
+  var returned_string = "";
+  word_array.forEach(function(word) {
+    returned_string += pigLatinWord(yChecker(word));
+  });
+  return returned_string.trim();
 };
 
+var pigLatinWord = function(word) {
+  for(var i=0; i<word.length; i++) {
+    var currentLetter = word.charAt(0);
+    if (vowelCheck(currentLetter)) {
+      break;
+    } else if (currentLetter.toLowerCase() === "q" && word.charAt(1).toLowerCase() === "u") {
+      word = word.slice(2, word.length);
+      word += "qu";
+    } else {
+      word = word.slice(1, word.length);
+      word += currentLetter;
+    }
+  }
+  return word += "ay ";
+}
+
+var wordSeparator = function(string) {
+  return string.split(" ");
+}
+
+
+var yChecker = function(string) {
+  if(string.charAt(0).toLowerCase() === "y") {
+    string = string.slice(1, string.length) + "y";
+  }
+  return string;
+}
+
 var vowelCheck = function(letter) {
-  return ["a", "e", "i", "o", "u"].indexOf(letter.toLowerCase()) !== -1;
+  return ["a", "e", "i", "o", "u", "y"].indexOf(letter.toLowerCase()) !== -1;
 };
